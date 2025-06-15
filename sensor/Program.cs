6adcfb12a4db270759d4835926e635991ddf0708
Israel Vitzman <iv058.l.v@gmail.com>
2025-06-15 15:50:27 +0300

    ver 4

--- sensor/Program.cs
+++ sensor/Program.cs
@@ -10,6 +10,7 @@ namespace sensor
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
--- sensor/sensor.cs
+++ sensor/sensor.cs
@@ -6,7 +6,71 @@ using System.Threading.Tasks;
 
 namespace sensor
 {
-    internal class sensor
+    internal class Sensor
     {
+        private string[] sensors;
+        private string[] rank;
+       
+
+        public Sensor()
+        {
+            sensors = new string[] { "Audio", "Thermal", "Pulse", "Motion", "Magnetic", "Signal", "Light" };
+            
+        }
+
+
+
+
+        public string Audio()
+        {
+            return "";
+        }
+
+
+        public string Thermal()
+        {
+            return "";
+        }
+
+
+        public string Pulse()
+        {
+            return "";
+        }
+
+
+        public string Motion()
+        {
+
+
+            return "";
+        }
+
+
+
+        public string Magnetic()
+        {
+
+
+            return "";
+        }
+
+
+        public string Signal()
+        {
+
+            return "";
+        }
+
+
+        public string Light()
+        {
+
+            return "";
+        }
+
+
+
+
     }
 }
