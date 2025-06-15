630bb3d32a9f53b6f8e43cf255678e79ac1a7c06
Israel Vitzman <iv058.l.v@gmail.com>
2025-06-15 23:19:57 +0300

    ver5

--- sensor/Activate.cs
+++ sensor/Activate.cs
@@ -10,13 +10,22 @@ namespace sensor
     internal class Activate
     {
         Agent agent = new Agent();
+        int count = 0;
+       
 
-        public void FindAgent()
-        {    
+        public void FindAgent(string sensorToPlayer)
+        {   //יכול להיות להיות בעיה שהוא יוריד ..
+            int countForSensorIrani = agent.GetAgentLengte();
             // לוגיקה לרוץ על הקאי שהוא סטרינג ולהשוות לסטרינג של השחקן מכניס אם קיים להוריד אותו מהדיקשנקי ולשים נקודה 
-            foreach (string sensor in agent.getAgent().Keys)
-            {
+            foreach (string sensor in agent.GetAgent().Keys)
+            { 
 
+                if (sensorToPlayer.Equals(sensor)) 
+                { 
+                    agent.GetAgent().Remove(sensor);
+                    count += 1;
+                    Console.WriteLine($"{countForSensorIrani}מתוך   {count}קלעת !! נשאר ");
+                }
                 
             }
 
