65aadead7fbf2fbd5c6c759644ec6a17c4dbfde3
Israel Vitzman <iv058.l.v@gmail.com>
2025-06-15 23:01:49 +0300

    ver3

---
+++ sensor/Activate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sensor
+{
+    internal class Activate
+    {
+        Agent agent = new Agent();
+
+        public void FindAgent()
+        {    
+            // לוגיקה לרוץ על הקאי שהוא סטרינג ולהשוות לסטרינג של השחקן מכניס אם קיים להוריד אותו מהדיקשנקי ולשים נקודה 
+            foreach (string sensor in agent.getAgent().Keys)
+            {
+
+                
+            }
+
+        }
+    }
+}
--- sensor/agent.cs
+++ sensor/agent.cs
@@ -15,8 +15,8 @@ namespace sensor
         Dictionary<String ,int> rank ;
         Dictionary<string, Func<string>> sensorActions;
 
+        Dictionary<string, Func<string>> iraniAgent;
 
-        private string[] agentIrani;
 
         public agent()
         {
@@ -41,9 +41,26 @@ namespace sensor
            };
         }   
         
-        public  void NewAgent()
+        public  void NewAgent(string name)
         {
+            iraniAgent = new Dictionary<string, Func<string>>();
+            int randomRank = random.Next(0, rank.Count);
 
+
+            List<string> keys = sensorActions.Keys.ToList();
+
+            for (int i = 0; i < randomRank; i++) 
+            {
+                int randSensorActions=random.Next(keys.Count);
+
+                
+                iraniAgent.Add(keys[randSensorActions], sensorActions[keys[randSensorActions]]);
+
+                //צריך לטפל במצב שהוא כבר קיים בתוך האירני 
+                // לבדוק שהקוד תקין מצד תחביר שלו
+
+
+            }
         }
     }
 }
