36b327b6feccd4b459bdb9302b1b8cb0faacc885
Israel Vitzman <iv058.l.v@gmail.com>
2025-06-15 13:31:49 +0300

    ver2

--- sensor/agent.cs
+++ sensor/agent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,32 @@ using System.Threading.Tasks;
 namespace sensor
 {
     internal class agent
-    {
+    {   
+        Random random= new Random();
+        private string[] rank;
+        Sensor sensor=new Sensor();
+        string [] agentIrani;
+        private Dictionary<string, Func<string>> sensorActions;
+
+
+        public agent()
+        {
+            rank = new string[] { "Foot Soldier", "Squad Leader", "Senior Commander", "Organization Leader" };
+            sensorActions = new Dictionary<string, Func<string>>()
+            {
+              { "Audio",sensor. Audio },
+              { "Thermal",sensor. Thermal},
+              { "Pulse",sensor. Pulse },
+              { "Motion", sensor.Motion },
+              { "Magnetic", sensor.Magnetic},
+              { "Signal", sensor.Signal },
+              { "Light", sensor.Light }
+            };
+        }   
+        
+        public  void NewAgent()
+        {
+
+        }
     }
 }
